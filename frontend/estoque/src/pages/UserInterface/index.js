91b322ad3a79e2735964f83d9b73956266c2080c
Elon <elonneto@outlook.com>
2020-04-20 17:52:59 -0300

    update

--- frontend/estoque/src/pages/UserInterface/index.js
+++ frontend/estoque/src/pages/UserInterface/index.js
@@ -7,24 +7,55 @@ import './userStyles.css';
 
 export default function UserInterface() {
     // eslint-disable-next-line
-    const [stock, setStock] = useState([{nome: 'RJ com capa', quantidade: 10},
-    {nome: 'Roteador aaaa', quantidade: 20},
-    {nome: 'Item', quantidade: 50},
-    {nome: 'Item', quantidade: 50},
-    {nome: 'Item', quantidade: 50},
-    {nome: 'Item', quantidade: 50},
-    {nome: 'Item', quantidade: 50},
-    {nome: 'Item', quantidade: 50},
-    {nome: 'Item', quantidade: 50}
+    const [stock, setStock] = useState([{nome: 'RJ com capa', quantidade: 10, id: 1},
+    {nome: 'Roteador aaaa', quantidade: 20, id: 2},
+    {nome: 'Item', quantidade: 50, id: 3},
+    {nome: 'Item', quantidade: 50, id: 4},
+    {nome: 'Item', quantidade: 50, id: 5},
+    {nome: 'Item', quantidade: 50, id: 6},
+    {nome: 'Item', quantidade: 50, id: 7},
+    {nome: 'Item', quantidade: 50, id: 8},
+    {nome: 'Item', quantidade: 50, id: 9}
     ]);
 
-    let renderItem = ({nome, quantidade}) => {
+    // eslint-disable-next-line
+    const [requests, setRequests] = useState([]);
+
+    let requestItem = (item) => {
+        let nome = item.nome;
+        let quantidade = item.quantidade;
+        let id = item.id;
+        switch(requests.length) {
+            case 0: 
+                setRequests([{ nome , quantidade, id }]);
+                break;
+
+            default:
+                let exists;
+                exists = requests.map((iten) => {
+                    if(iten.id === item.id) {
+                        return true;
+                    }else {return false;}
+                });
+
+                if(exists === false) {
+                    setRequests([...requests, { nome , quantidade, id }]);
+
+                }
+
+        }
+
+        console.log(requests)
+
+    }
+
+    let renderItem = (item) => {
         return (
         <div className="ItensDiv">
             <div className="Itens">
-                <div className="ItemText">{nome}</div>
-                <div className="ItemQuant">{quantidade}</div>
-                <FiSend className="Request" size={20} color="#1134e7" />
+                <div className="ItemText">{item.nome}</div>
+                <div className="ItemQuant">{item.quantidade}</div>
+                <button className="Request" onClick={() => {requestItem(item)}}><FiSend size={20} color="#1134e7" /></button>
             </div>
         </div>
 
@@ -47,7 +78,7 @@ export default function UserInterface() {
                     <div>Ações</div>
                 </div>
             </div>
-            <FlatList className="StockList" list={stock} renderItem={renderItem} />
+            <FlatList list={stock} renderItem={renderItem} />
             <div>Pedidos:</div>
         </div>
     );
